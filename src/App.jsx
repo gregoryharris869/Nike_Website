ab0fa6b8d862e181439d9b2009d69fb00eb1beb1
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-11-15 14:58:14 -0600

    Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => (
+  <main className="relative">
+    {/* Nav */}
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      {/* Hero */}
+    </section>
+    <section className="padding">{/* PopularProducts */}</section>
+    <section className="padding">{/* SuperQuality */}</section>
+    <section className="padding-x py-10">{/* Services */}</section>
+    <section className="padding">{/* SpecialOffers */}</section>
+  </main>
+);
+
+export default App;
