3e443063d97f85fcb4e891667fdaebc944e1e590
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-11-16 23:53:54 -0600

    Update PopularProducts.jsx

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -3,7 +3,7 @@ import PopularProductCard from "../components/PopularProductCard";
 
 const PopularProducts = () => {
   return (
-    <section id="products" className="max-container max-sm:mt-5 ">
+    <section id="products" className="py-16 max-container max-sm:mt-5 ">
       <div className="flex flex-col justify-start gap-5">
         <h2 className="text-4xl font-bold font-palanquin">
           Our <span className="text-coral-red"> Popular </span> Products
